6a184cf889ca9317741306260ef0f3d719346041
OSercy <OSercy@users.noreply.github.com>
2014-09-21 22:42:10 +1100

    Update cacheSolve.r

--- cacheSolve.r
+++ cacheSolve.r
@@ -1,6 +1,7 @@
 cacheSolve <- function(x,...){
   inv <- x$getinv()
   if(!is.null(inv)){
+    //if the inverse matrix is already in cache
     message("getting cached inverse matrix")
     return(inv)
   }
