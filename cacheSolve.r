1bb64653676f911ae3ebc302f4ea7eecca040796
OSercy <OSercy@users.noreply.github.com>
2014-09-21 22:30:56 +1100

    Create cacheSolve.r

---
+++ cacheSolve.r
@@ -0,0 +1,11 @@
+cacheSolve <- function(x,...){
+  inv <- x$getinv()
+  if(!is.null(inv)){
+    message("getting cached inverse matrix")
+    return(inv)
+  }
+  data<- x$get()
+  inv <- solve(data,...)
+  x$setinv(inv)
+  inv
+}
